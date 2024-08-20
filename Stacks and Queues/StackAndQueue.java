721e772bb69299dd9100c8e98dbb8d8d18477cee
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-08-20 13:06:05 -0400

    add towers of hanoi

--- Stacks and Queues/StackAndQueue.java
+++ Stacks and Queues/StackAndQueue.java
@@ -1,85 +1,85 @@
-import java.util.EmptyStackException;
-import java.util.NoSuchElementException;
+// import java.util.EmptyStackException;
+// import java.util.NoSuchElementException;
 
-class Stack<T> {
-    private Node<T> top;
+// class Stack<T> {
+//     private Node<T> top;
 
-    public Stack(T data) {
-        Node<T> top = new Node(data);
-        this.top = top;
-    }
+//     public Stack(T data) {
+//         Node<T> top = new Node(data);
+//         this.top = top;
+//     }
 
-    public T pop() {
-        if (top == null)
-            throw new EmptyStackException();
-        T item = (T) top.getData();
-        top = top.next;
-        return item;
-    }
+//     public T pop() {
+//         if (top == null)
+//             throw new EmptyStackException();
+//         T item = (T) top.getData();
+//         top = top.next;
+//         return item;
+//     }
 
-    public void push(T data) {
-        Node dataNode = new Node(data);
-        dataNode.next = top;
-        top = dataNode;
-    }
+//     public void push(T data) {
+//         Node dataNode = new Node(data);
+//         dataNode.next = top;
+//         top = dataNode;
+//     }
 
-    public T peek() {
-        return (T) top.data;
-    }
+//     public T peek() {
+//         return (T) top.data;
+//     }
 
-    public boolean isEmpty() {
-        return top == null;
-    }
-}
+//     public boolean isEmpty() {
+//         return top == null;
+//     }
+// }
 
-class Queue<T> {
-    T data;
-    Node<T> first;
-    Node<T> last;
+// class Queue<T> {
+//     T data;
+//     Node<T> first;
+//     Node<T> last;
 
-    public Queue(T data) {
-        this.data = data;
-    }
+//     public Queue(T data) {
+//         this.data = data;
+//     }
 
-    public void enqueue(T data) {
-        Node<T> dataNode = new Node(data);
-        if (last != null) {
-            last.next = dataNode;
-        }
-        last = dataNode;
-        if (first == null) {
-            first = last;
-        }
-    }
+//     public void enqueue(T data) {
+//         Node<T> dataNode = new Node(data);
+//         if (last != null) {
+//             last.next = dataNode;
+//         }
+//         last = dataNode;
+//         if (first == null) {
+//             first = last;
+//         }
+//     }
 
-    public T dequeue() {
-        if (first == null)
-            throw new NoSuchElementException();
-        Node<T> item = first;
-        first = first.next;
-        return item.getData();
-    }
+//     public T dequeue() {
+//         if (first == null)
+//             throw new NoSuchElementException();
+//         Node<T> item = first;
+//         first = first.next;
+//         return item.getData();
+//     }
 
-    public T peek() {
-        if (first == null)
-            throw new NoSuchElementException();
-        return first.getData();
-    }
+//     public T peek() {
+//         if (first == null)
+//             throw new NoSuchElementException();
+//         return first.getData();
+//     }
 
-    public boolean isEmpty() {
-        return first == null;
-    }
-}
+//     public boolean isEmpty() {
+//         return first == null;
+//     }
+// }
 
-class Node<T> {
-    T data;
-    Node<T> next;
+// class Node<T> {
+//     T data;
+//     Node<T> next;
 
-    public Node(T data) {
-        this.data = data;
-    }
+//     public Node(T data) {
+//         this.data = data;
+//     }
 
-    public T getData() {
-        return data;
-    }
-}
+//     public T getData() {
+//         return data;
+//     }
+// }
