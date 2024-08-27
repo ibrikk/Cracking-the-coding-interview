b9c70ca0e4bc3e9b43927f85c998e93360d4ef74
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-08-26 20:50:40 -0400

    add n queens problem

--- Ch.8-Recursion And Dynamic Programming/EightQueens.java
+++ Ch.8-Recursion And Dynamic Programming/EightQueens.java
@@ -36,6 +36,7 @@ public class EightQueens {
       results.add(columns.clone()); // Found valid placement
     } else {
       for (int col = 0; col < GRID_SIZE; col++) {
+        // Look into next row
         if (checkValid(columns, row, col)) {
           columns[row] = col; // Place Queen
           placeQueens(row + 1, columns, results);
@@ -45,20 +46,18 @@ public class EightQueens {
   }
 
   /*
-   * Check if (rowl, column!) is a valid spot for a queen by checking if there is
-   * a
+   * Check if (rowl, column) is a valid spot for a queen by checking if there is a
    * queen in the same column or diagonal. We don't need to check it for
-   * queens in
-   * the same row because the calling placeQueen only attempts to place one
+   * queens in the same row because the calling placeQueen only attempts to place one
    * queen at a time. We know this row is empty.
    */
-  private static boolean checkValid(Integer[] columns, int row1, int column1) {
-    for (int row2 = 0; row2 < row1; row2++) {
-      int column2 = columns[row2];
-      /* Check if (row2, columns2) invalidates (row1, column1) as a queen spot */
+  private static boolean checkValid(Integer[] columns, int row, int column) {
+    for (int rowOld = 0; rowOld < row; rowOld++) {
+      int columnOld = columns[rowOld];
+      /* Check if (rowOld, columnsOld) invalidates (row, column) as a queen spot */
 
       /* Check if rows have a queen in the same column */
-      if (column1 == column2) {
+      if (column == columnOld) {
         return false;
       }
 
@@ -66,10 +65,10 @@ public class EightQueens {
        * Check diagonals: if the distance between the columns equals the distance
        * between the rows, then they're in the same diagonal.
        */
-      int columnDistance = Math.abs(column2 - column1);
+      int columnDistance = Math.abs(columnOld - column);
 
-      /* row1 > row2, so no need for abs */
-      int rowDistance = row1 - row2;
+      /* row > rowOld, so no need for abs */
+      int rowDistance = row - rowOld;
       if (columnDistance == rowDistance) {
         return false;
       }
