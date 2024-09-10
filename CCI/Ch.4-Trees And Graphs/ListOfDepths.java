f9e6c5dbf82bbfa0aefd9c292a9009cd69b01c19
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-09-10 13:55:36 -0400

    list of depths

--- CCI/Ch.4-Trees And Graphs/ListOfDepths.java
+++ CCI/Ch.4-Trees And Graphs/ListOfDepths.java
@@ -9,7 +9,9 @@ public class ListOfDepths {
     node.left.left = new TreeNode(4);
     node.left.right = new TreeNode(5);
     ArrayList<LinkedList<TreeNode>> result = createLevelLinkedList(node);
+    ArrayList<LinkedList<TreeNode>> result2 = createLevelLinkedListBFS(node);
     System.out.println(result);
+    System.out.println(result2);
   }
 
   private static void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
@@ -37,6 +39,31 @@ public class ListOfDepths {
     ArrayList<LinkedList<TreeNode>> lists = new ArrayList<>();
     createLevelLinkedList(root, lists, 0);
     return lists;
+  }
+
+  // BFS
+  private static ArrayList<LinkedList<TreeNode>> createLevelLinkedListBFS(TreeNode root) {
+    ArrayList<LinkedList<TreeNode>> result = new ArrayList<>();
+    /* Visit the root */
+    LinkedList<TreeNode> current = new LinkedList<>();
+    if (root != null) {
+      current.add(root);
+    }
+    while (current.size() > 0) {
+      result.add(current); // Add previous level
+      LinkedList<TreeNode> parents = current; // Go to next level
+      current = new LinkedList<>();
+      for (TreeNode parent : parents) {
+        /* Visit the children */
+        if (parent.left != null) {
+          current.add(parent.left);
+        }
+        if (parent.right != null) {
+          current.add(parent.right);
+        }
+      }
+    }
+    return result;
   }
 
 }
